e6dbffb94e44e3f1d2940d2a123205a61551afb6
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2021-05-31 23:55:08 -0400

    5/31/2021 Update 2
    It's very late rn. Added a very experimental and very WIP economy system, as well as now it's required to have the role "Administrators" or "Moderators" to do $b kick, $b ban, and $b unban.

--- bmain-public.py
+++ bmain-public.py
@@ -37,11 +37,13 @@ async def secretmegumintestcommand(ctx):
  await ctx.send("https://hg1.funnyjunk.com/large/pictures/04/a5/04a5ea_6343222.jpg")
 
 @client.command()
+@commands.has_any_role("Administrators", "Moderators")
 async def kick(ctx, member : discord.Member, *, reason=None):
  await member.kick(reason=reason)
  await ctx.send(f"{member} has been kicked!")
 
 @client.command()
+@commands.has_any_role("Administrators", "Moderators")
 async def ban(ctx, member : discord.Member, *, reason=None):
  await member.ban(reason=reason)
  await ctx.send(f"{member.mention} has been banned!")
@@ -60,6 +62,7 @@ async def waifu(ctx):
  await ctx.send(waifu_id)
 
 @client.command()
+@commands.has_any_role("Administrators", "Moderators")
 async def unban(ctx, user: discord.User):
  guild = ctx.guild
  if ctx.author.guild_permissions.ban_members:
@@ -74,6 +77,53 @@ async def apphookup(ctx):
 
 @client.command()
 async def info(ctx):
- await ctx.send('**Commands:**\n\n$b kick {member} - kicks member\n$b ban {member} - bans member\n$b unban {memberid} - unbans member\n$b waifu - sends a pic of a waifu\n$b meme - sends a meme\n$b greentext - sends a 4chan post\n$b apphookup - sends a deal on apps/games\n$b hello - say hello to the bot\n\n\n**Source Code: **https://github.com/zachary7829/BananaBot')
+ await ctx.send('**Commands:**\n\n$b kick {member} - kicks member\n$b ban {member} - bans member\n$b unban {member} - unbans member\n$b balance - shows your balance\n$b beg - beg for money\n$b waifu - sends a pic of a waifu\n$b meme - sends a meme\n$b greentext - sends a 4chan post\n$b appdeal - sends a deal on apps/games\n$b hello - say hello to the bot\n\n\n**Source Code: **https://github.com/zachary7829/BananaBot')
+
+@client.command()
+async def balance(ctx):
+ await open_account(ctx.author)
+ user = ctx.author
+ users = await get_bank_data()
+
+ wallet_amt = users[str(user.id)]["wallet"]
+ bank_amt = users[str(user.id)]["bank"]
+
+ em = discord.Embed(title = f"{ctx.author.name}'s balance",color = discord.Color.blue())
+ em.add_field(name = "Wallet",value = wallet_amt)
+ em.add_field(name = "Bank balance",value = bank_amt)
+ await ctx.send(embed = em)
+
+@client.command()
+async def beg(ctx):
+ await open_account(ctx.author)
+ users = await get_bank_data()
+ user = ctx.author
+ earnings = random.randrange(101)
+ await ctx.send(f"Someone gave you {earnings} coins!")
+ users[str(user.id)]["wallet"] += earnings
+ with open("mainbank.json","w") as f:
+  json.dump(users,f)
+ 
+
+async def open_account(user):
+
+ users = await get_bank_data()
+
+ if str(user.id) in users:
+  return False
+ else:
+  users[str(user.id)] = {}
+  users[str(user.id)]["wallet"] = 0
+  users[str(user.id)]["bank"] = 0
+
+ with open("mainbank.json","w") as f:
+  json.dump(users,f)
+ return True
+
+async def get_bank_data():
+ with open("mainbank.json","r") as f:
+  users = json.load(f)
+
+ return users
 
 client.run()
